1e0582782cf81699991fcbe2cd2cf14aaf8ae374
tw-life <stupid0912@gmail.com>
2025-12-25 20:47:33 +0800

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDMJrxOpvGfgsz-U9JVMhdXwX26m4qgj5w",

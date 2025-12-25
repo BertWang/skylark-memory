4942552fa5dba7553cc23a10efc49a5d5e138804
tw-life <stupid0912@gmail.com>
2025-12-25 20:46:16 +0800

    test

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDMJrxOpvGfgsz-U9JVMhdXwX26m4qgj5w",
-    authDomain: "skylark-forms.firebaseapp.com",
-    projectId: "skylark-forms",
-    storageBucket: "skylark-forms.firebasestorage.app",
-    messagingSenderId: "646072999112",
-    appId: "1:646072999112:web:7eea7d45a7f62180fb59fd"
+  apiKey: "AIzaSyDMJrxOpvGfgsz-U9JVMhdXwX26m4qgj5w",
+  authDomain: "skylark-forms.firebaseapp.com",
+  projectId: "skylark-forms",
+  storageBucket: "skylark-forms.firebasestorage.app",
+  messagingSenderId: "646072999112",
+  appId: "1:646072999112:web:7eea7d45a7f62180fb59fd"
 };
 
 // 初始化
